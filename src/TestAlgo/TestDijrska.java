8ec565f0e63225405cee8ad87f4c7ecb62124859
GraceGR <46426990+GraceGR@users.noreply.github.com>
2019-11-21 16:16:50 +0100

    Revert "Again"
    This reverts commit a0459e5ac0debbd927665dddeb3b77a89b0ebfa8.

--- src/TestAlgo/TestDijrska.java
+++ src/TestAlgo/TestDijrska.java
@@ -12,15 +12,14 @@
 package TestAlgo;
 
 
-/*import org.jgrapht.*;
-import org.jgrapht.alg.connectivity.*;
-import org.jgrapht.alg.interfaces.ShortestPathAlgorithm.*;
-import org.jgrapht.alg.interfaces.*;
-import org.jgrapht.alg.shortestpath.*;
+import org.jgrapht.*;
+import org.jgrapht.generate.*;
 import org.jgrapht.graph.*;
-*/
-import java.util.*;
+import org.jgrapht.traverse.*;
+import org.jgrapht.util.*;
 
+import java.util.*;
+import java.util.function.*;
 /*
  *
  * @author GRAZIA GIULIA
@@ -29,12 +28,38 @@ public class TestDijrska {
     
     public static void main(String[] args)
     {
-        
-        System.out.println("Yes");
-        
-      
+        // Create the VertexFactory so the generator can create vertices
+        Supplier<String> vSupplier = new Supplier<String>()
+        {
+            private int id = 0;
 
-      
+            @Override
+            public String get()
+            {
+                return "v" + id++;
+            }
+        };
+
+        // Create the graph object
+        Graph<String, DefaultEdge> completeGraph =
+            new SimpleGraph<>(vSupplier, SupplierUtil.createDefaultEdgeSupplier(), false);
+
+        // Create the CompleteGraphGenerator object
+        CompleteGraphGenerator<String, DefaultEdge> completeGenerator =
+            new CompleteGraphGenerator<>(10);
+
+        // Use the CompleteGraphGenerator object to make completeGraph a
+        // complete graph with [size] number of vertices
+        completeGenerator.generateGraph(completeGraph);
+
+        // Print out the graph to be sure it's really complete
+        Iterator<String> iter = new DepthFirstIterator<>(completeGraph);
+        while (iter.hasNext()) {
+            String vertex = iter.next();
+            System.out.println(
+                "Vertex " + vertex + " is connected to: "
+                    + completeGraph.edgesOf(vertex).toString());
+        }
     }
 }
 
