f647bf7d8ed2a261b4e8bc79d86a9b5e59139993
AndreaCroc <andrea.croc@yahoo.fr>
2019-12-11 11:36:36 +0100

    Update EcouteurSouris.java

--- src/Vue/EcouteurSouris.java
+++ src/Vue/EcouteurSouris.java
@@ -85,9 +85,6 @@ public class EcouteurSouris extends MouseAdapter {
                                     + 25;
                             PointInteret pi = p.getPtI();
                             //Si le clic se trouve sur une figure d un point d interet
-
-                            if ((x >= nvXpt - 6) && (x <= nvXpt + 6)
-                                    && (y >= nvYpt - 6) && (y <= nvYpt + 6)) {
                             if ((x >= nvXpt - 5) && (x <= nvXpt + 5)
                                     && (y >= nvYpt - 5) && (y <= nvYpt + 5)) {
                                 if (listePtI != null && !listePtI.isEmpty()) {
